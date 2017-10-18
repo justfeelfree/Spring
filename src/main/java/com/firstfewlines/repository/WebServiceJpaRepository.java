9f8fe1fc1f20160563ab7fa2450a67e7a8219564
CMX\knigmatullin <knigmatullin@cinimex.ru>
2017-10-18 14:50:41 +0300

    fixed bug with native query

--- src/main/java/com/firstfewlines/repository/WebServiceJpaRepository.java
+++ src/main/java/com/firstfewlines/repository/WebServiceJpaRepository.java
@@ -17,7 +17,7 @@ public interface WebServiceJpaRepository extends JpaRepository<WebService, Integ
     @Query("SELECT w FROM WebService w WHERE w.price >= 100")
     List<WebService> simpleQuery();
 
-    @Query(value = "SELECT * FROM public.\"web_service\" WHERE name like '%11'", nativeQuery = true)
+    @Query(value = "SELECT * FROM public.\"web_service\" WHERE name like '%13'", nativeQuery = true)
     List<WebService> nativeQuery();
 
 
